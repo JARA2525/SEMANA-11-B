5facc006022af4cf921497676ebf6c2cce21def9
JOHN JAIRO JARA <jj.jarao@uea.edu.ec>
2026-03-01 13:03:07 -0500

    Implementación de la clase Producto con encapsulación

---
+++ MODELOS/producto.py
@@ -0,0 +1,35 @@
+class Producto:
+    def __init__(self, id_producto, nombre, cantidad, precio):
+        self.__id = id_producto
+        self.__nombre = nombre
+        self.__cantidad = cantidad
+        self.__precio = precio
+
+    # Getters
+    def get_id(self):
+        return self.__id
+
+    def get_nombre(self):
+        return self.__nombre
+
+    def get_cantidad(self):
+        return self.__cantidad
+
+    def get_precio(self):
+        return self.__precio
+
+    # Setters
+    def set_cantidad(self, nueva_cantidad):
+        self.__cantidad = nueva_cantidad
+
+    def set_precio(self, nuevo_precio):
+        self.__precio = nuevo_precio
+
+    # Convertir objeto a diccionario (para JSON)
+    def to_dict(self):
+        return {
+            "id": self.__id,
+            "nombre": self.__nombre,
+            "cantidad": self.__cantidad,
+            "precio": self.__precio
+        }
